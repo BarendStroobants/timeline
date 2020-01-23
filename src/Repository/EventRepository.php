5b6a960975aff026fb2c5770ec08cb2294874048
barend <barend.stroobants@gmail.com>
2020-01-23 13:51:41 +0100

    merging and styling login

--- src/Repository/EventRepository.php
+++ src/Repository/EventRepository.php
@@ -50,12 +50,8 @@ class EventRepository extends ServiceEntityRepository
     }
     */
 
-<<<<<<< HEAD
     public function getUsefulDataArray (Person $person):array{
-=======
-    public function getUsefulDataArray(Person $person)
-    {
->>>>>>> ef14132b9c6583299b09844c2e7a68e2a3ace471
+
         $events = $this->findBy([
             'person' => $person,
         ]);
@@ -75,7 +71,7 @@ class EventRepository extends ServiceEntityRepository
             $minuteArray[$indexCounter][$counter]['activity'] = $event->getActivity();
             $minuteArray[$indexCounter][$counter]['date'] = $event->getStart()->format('d');
             $counter++;
-            if ()
+
 
 
         }
